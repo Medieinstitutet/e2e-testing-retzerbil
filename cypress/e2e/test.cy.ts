69c7a093f7fbf1485de5259126ccd5cb93e0ccef
Andreas Retzius <andreasretzius@gmail.com>
2024-04-29 12:57:05 +0200

    format

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -19,32 +19,33 @@ describe('Intercepting omdb call testing', () => {
     cy.get(".movie > h3:first").should("have.text", "yeah");
   });
 
-it("should not show error when api connection fails", ()=>{
-  cy.intercept("http://omdbapi.com/*",{Body:[
-    {Title:"DU SUGER"},{Title:"hahahahah"}
-  ],
-  statusCode:500})
+  it("should not show error when api connection fails", () => {
+    cy.intercept("http://omdbapi.com/*", {
+      Body: [
+        { Title: "DU SUGER" }, { Title: "hahahahah" }
+      ],
+      statusCode: 500
+    })
 
-  cy.get("#searchText").type("yeah{enter}");
-  cy.get("p").should("have.text", "Inga sökresultat att visa")
-})
-
-
-it("should display error if movie doesnt exist", ()=>{
-  cy.intercept("http://omdbapi.com/*",{Body:[{},{}]})
-
-  cy.get("#searchText").type("thismoviedoesntexisthahaha{enter}");
-  cy.get("p").should("have.text", "Inga sökresultat att visa");
-})
+    cy.get("#searchText").type("yeah{enter}");
+    cy.get("p").should("have.text", "Inga sökresultat att visa")
+  })
 
 
-it("should display an error if movietitle is empty", ()=>{
-  cy.intercept("http:/omdbapi.com/*",{Body:[{},{}]})
+  it("should display error if movie doesnt exist", () => {
+    cy.intercept("http://omdbapi.com/*", { Body: [{}, {}] })
 
-  cy.get("#searchText").type("{enter}");
-  cy.get("#searchText").should("be.empty");
-  cy.get("p").should("have.text","Inga sökresultat att visa");
+    cy.get("#searchText").type("thismoviedoesntexisthahaha{enter}");
+    cy.get("p").should("have.text", "Inga sökresultat att visa");
+  })
 
-})
 
+  it("should display an error if movietitle is empty", () => {
+    cy.intercept("http:/omdbapi.com/*", { Body: [{}, {}] })
+
+    cy.get("#searchText").type("{enter}");
+    cy.get("#searchText").should("be.empty");
+    cy.get("p").should("have.text", "Inga sökresultat att visa");
+
+  })
 });
