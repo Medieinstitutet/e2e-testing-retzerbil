7cc0a02ed796b3eec317b608d6e9808ac8d1eb77
Andreas Retzius <andreasretzius@gmail.com>
2024-04-29 11:47:09 +0200

    Test for interjecting api call

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,10 +1,22 @@
 //http://www.omdbapi.com/?i=tt3896198&apikey=6424148a
 
 describe('Intercepting omdb call testing', () => {
-  it('should intercept omdb request', () => {
-    cy.visit("http://localhost:5173/")
-    cy.intercept("GET","http://www.omdbapi.com/?i=tt3896198&apikey=6424148a",{body:[{Title:"yeah",Year:"2053"},{Title:"movie",Year:1993}]})
-  
+  beforeEach(() => {
+    cy.visit("/");
   })
 
-})
+  it('should intercept omdb request', () => {
+    cy.intercept("http://omdbapi.com/*",
+      {
+        Search: [
+          { Title: "yeah", Year: "2053", imdbID: "15", Type: "movie", Poster: "google.com" },
+          { Title: "movie", Year: "1993", imdbID: "15", Type: "movie", Poster: "google.com" }]
+      })
+
+    cy.get("#searchText").type("yeah{enter}");
+
+    cy.get(".movie").should("have.length", 2);
+    cy.get(".movie > h3:first").should("have.text", "yeah");
+  });
+
+});
